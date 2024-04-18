0db778b76e4ad76339e52e39353393e2397506b2
kathryn basile <80859204+kbasil3@users.noreply.github.com>
2024-04-18 10:55:19 -0400

    Add files via upload

---
+++ rest/rest/rest/restApp.swift
@@ -0,0 +1,17 @@
+//
+//  restApp.swift
+//  rest
+//
+//  Created by Katie on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
